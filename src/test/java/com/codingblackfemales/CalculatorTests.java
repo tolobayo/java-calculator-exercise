7a39b3c6cd7c9788d588fb761440bf3c4f6e9aee
omaneh <122321976+omaneh@users.noreply.github.com>
2024-11-21 20:22:40 -0500

    Added test cases for multiplication.

--- src/test/java/com/codingblackfemales/CalculatorTests.java
+++ src/test/java/com/codingblackfemales/CalculatorTests.java
@@ -3,6 +3,8 @@ package com.codingblackfemales;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigInteger;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DisplayName(value = "The Calculator should")
@@ -39,5 +41,39 @@ public class CalculatorTests {
 
         assertEquals(0, quotient);
     }
-    
+
+    // Multiplication Test Cases
+    // Multiply by 1
+    // Multiply by 0
+    // Two different numbers
+
+    @Test
+    @DisplayName("Multiplying value by 1")
+    public void testMultiplicationByValueOne() {
+        final Integer product = Calculator.multiply(1, 2);
+        assertEquals(2, product);
+    }
+
+    @Test
+    @DisplayName("Multiplying value by 0")
+    public void testMultiplicationByValueZero() {
+        final Integer product = Calculator.multiply(0, 94);
+        assertEquals(0, product);
+    }
+
+    @Test
+    @DisplayName("Multiplying two different values")
+    public void testMultiplication() {
+        final Integer product = Calculator.multiply(19, 4);
+        assertEquals(76, product);
+    }
+
+// Highest value: 2147483647
+//    @Test
+//    @DisplayName("Multiplying value to achieve above integer limit")
+//    public void testMultiplicationExceedIntegerLimit() {
+//        final BigInteger product = (BigInteger) Calculator.multiply(50000, 50000);
+//        assertEquals(2500000000, product);
+//    }
+//
 }
